f63e88bbf48c19ad6e7d12f9b772459774bb6952
st3penta <71923697+st3penta@users.noreply.github.com>
2020-12-05 18:40:35 +0100

    fixed getFacilities test

--- facility/getFacilities/test/integration/index.test.js
+++ facility/getFacilities/test/integration/index.test.js
@@ -6,6 +6,6 @@ const getFacilities = require('../../index')
 describe('getFacilities integration tests', function () {
   it('should return an array of facilities', async () => {
     const facilitiesList = await getFacilities.main()
-    expect(Array.isArray(facilitiesList.payload)).toBe(true)
+    expect(Array.isArray(facilitiesList.facilities)).toBe(true)
   })
 })
