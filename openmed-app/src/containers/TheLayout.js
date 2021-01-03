abceb2233be476316cd7fec5efd5d698eb737216
st3penta <71923697+st3penta@users.noreply.github.com>
2021-01-03 19:17:34 +0100

    added integration of keycloak auth status in axios instance

--- openmed-app/src/containers/TheLayout.js
+++ openmed-app/src/containers/TheLayout.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { TheContent, TheSidebar, TheFooter, TheHeader } from './index'
+import { useKeycloak } from '@react-keycloak/web'
+import { apiServer } from '../api/config'
 
 const TheLayout = () => {
+  const { keycloak } = useKeycloak()
+
+  useEffect(() => {
+    apiServer.interceptors.request.use(
+      function (config) {
+        if (keycloak.tokenParsed) {
+          config.headers.Authorization = 'Bearer ' + keycloak.token
+        }
+        return config
+      },
+
+      function (error) {
+        return Promise.reject(error)
+      }
+    )
+  }, [keycloak])
+
   return (
     <div>
       <div className="c-app c-default-layout">
